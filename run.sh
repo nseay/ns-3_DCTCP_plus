81b9f85a8684ee80f92221cd0cf2ad59ac779c14
Nick Seay <nicholasseay16@gmail.com>
2021-05-15 13:54:29 -0700

    reconsolidated duplicate dctcp simulations into scratch-simulator.cc. now can run experiment using run.sh

--- run.sh
+++ run.sh
@@ -1,21 +1,16 @@
 #!/bin/bash
 [ ! -d outputs ] && mkdir outputs
 
-TIME=60
-BWNET=10
-# TODO: If you want the Base RTT to be 40ms what should the delay on each
-# link be?  Set this value correctly.
-DELAY=__FILL_IN_HERE__
-
-for QSIZE in 20 100; do
-    DIR=outputs/bb-q$QSIZE
+NUMFLOWS=20
+for TCPTYPE in "TcpDctcp"; do
+    DIR=outputs/
     [ ! -d $DIR ] && mkdir $DIR
 
     # Run the NS-3 Simulation
-    ./waf --run "scratch/bufferbloat --bwNet=$BWNET --delay=$DELAY --time=$TIME --maxQ=$QSIZE"
+    ./waf --run "scratch/scratch-simulator --outputFilePath=$DIR --numFlows=$NUMFLOWS --enableSwitchEcn=$true"
     # Plot the trace figures
-    python3 scratch/plot_bufferbloat_figures.py --dir $DIR/
+    # python3 scratch/plot_bufferbloat_figures.py --dir $DIR/
 done
 
 echo "Simulations are done! Results can be retrieved via the server"
-python3 -m http.server
+# python3 -m http.server
